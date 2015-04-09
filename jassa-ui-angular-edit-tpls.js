282a26d1573ebebd991b5623d5343db1143d4e39
cstadler <cstadler@informatik.uni-leipzig.de>
2015-04-09 17:16:11 +0200

    Updating version 0.9.0-SNAPSHOT with tag v0.9.0-SNAPSHOT

--- jassa-ui-angular-edit-tpls.js
+++ jassa-ui-angular-edit-tpls.js
@@ -2,7 +2,7 @@
  * jassa-ui-angular-edit
  * https://github.com/GeoKnow/Jassa-UI-Angular
 
- * Version: 0.9.0-SNAPSHOT - 2015-03-25
+ * Version: 0.9.0-SNAPSHOT - 2015-04-09
  * License: BSD
  */
 angular.module("ui.jassa.edit", ["ui.jassa.edit.tpls", "ui.jassa.geometry-input","ui.jassa.rdf-term-input","ui.jassa.rex","ui.jassa.sync"]);
